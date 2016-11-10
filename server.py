a56d80011d4f3cb5deb4bcb9906638d89248fe9f
Miguel Sánchez de León Peque <peque@neosit.es>
2016-11-10 19:22:23 +0100

    Make server parameters configurable

--- server.py
+++ server.py
@@ -118,4 +118,8 @@ def _catch_all(path):
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, threaded=True, debug=True)
+    config = configparser.ConfigParser()
+    config.read('server.ini')
+    app.run(host=config['server']['host'],
+            port=int(config['server']['port']),
+            threaded=True, debug=True)
